eb55e22265219b3c760b37a0b0981ef3cf1226f4
SYJINTW <yuanjiun911@outlook.com>
2020-04-22 21:51:27 +0800

    add file I/O in 'add' function in 'view' function in 'delete' function

--- pymoney.py
+++ pymoney.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import sys
+import re
+import os.path
 
 #reset
 #=======================================================================================
 record = [['a',20],['a',30],['a',20],['a',20],['a',30]]
+file_path = '../record.txt'
 
 
 
@@ -12,64 +15,104 @@ record = [['a',20],['a',30],['a',20],['a',20],['a',30]]
 def divide():                                                   #divide line function
     print('='*40)
 
+def read_balance():
+    with open(file_path, 'r') as fh:
+        balance_in_file = int(fh.readline())
+    return balance_in_file
+
+def write_balance(balance):
+    try:
+        content = []
+        with open(file_path, 'r') as fh:
+            content = fh.readlines()
+    except FileNotFoundError:
+        pass
+    finally:
+        with open(file_path, 'w') as fh:
+            fh.write(str(balance)+'\n')
+            fh.writelines(content[1:])
 
 #user input 'add'
 def user_add(user_input):
-    global balance                                              #set 'balance' in the func global
+    balance = read_balance()                                             #set 'balance' in the func global
     error_exist = False
     user_input_comma = user_input.split(',')                    #set list split by ','
-    for i in user_input_comma:
-        try:
-            i = i.split()
-            i[1] = int(i[1])
-            balance += i[1]
-            record.append(i)                                   #append user input to record list
-        except IndexError:
-            error_exist = True
-            sys.stderr.write(f'Invalid format. Fail to add a record {i}.\n')
-        except ValueError:
-            sys.stderr.write(f'Invalid value for money. Fail to add a record {i}.\n')
+    with open(file_path, 'a') as fh:
+        for i in user_input_comma:
+            try:
+                i = i.split()
+                i[1] = int(i[1])
+                balance += i[1]
+                fh.write(str(i[0])+':'+str(i[1])+'\n')      #append user input to record file
+            except IndexError:
+                error_exist = True
+                sys.stderr.write(f'Invalid format. Fail to add a record {i}.\n')
+            except ValueError:
+                error_exist = True
+                sys.stderr.write(f'Invalid value for money. Fail to add a record {i}.\n')
+    write_balance(balance)
+
     if error_exist == True:
         sys.stderr.write('The format of a record should be like this: breakfast -50\n\n')
     else:
         pass
+
     return 0
 
 #user input 'view'
 def user_view():
     print('{:<20}{:<20}'.format('Description', 'Amount'))
     divide()
-    for i, j in record:
-        print('{:<20}{:<20}'.format(i, j))
+    with open(file_path, 'r') as fh:
+        for line in fh.readlines()[1:]:
+            record = line.split(':')
+            sys.stdout.write(f'{record[0]:<20}{record[1]}')
     divide()
+    balance = read_balance()
     print(f'Now you have {balance} dollars.')
     return 0
 
 #user input 'delete'
-def user_delete(s):
-    global balance                                              #set 'balance' in the func global
-    s = s.split()
-    s[1] = int(s[1])
-    if s in record:                                             #check if value exist
-        last = len(record) - record[::-1].index(s) - 1          #find the last exist value index
-        balance -= s[1]                                         #count balance
-        del(record[last])                                       #delete value
-    else:
-        pass
+def user_delete(user_input):
+    lines = []
+    del_record = []
+    balance = read_balance()                                              #set 'balance' in the func global
+    s = user_input.split()
+    with open(file_path, 'r') as fh:
+        for line in fh.readlines()[1:]:
+            lines.append(re.split(':|\n', line)[:2])
 
+    if s in lines:                                             #check if value exist
+        last_index = len(lines) - lines[::-1].index(s) - 1          #find the last exist value index
+        s[1] = int(s[1])
+        balance -= s[1]                                         #count balance
+        del(lines[last_index])                                       #delete value
+
+        del_record.append(str(balance) + '\n')
+        for i, j in lines:
+            s_format = str(i) + ':' + str(j) + '\n'
+            del_record.append(s_format)
+        with open(file_path, 'w') as fh:
+            fh.writelines(del_record)
+    else:
+        print(f'There is no record with ({s[0]} {s[1]}). Fail to delete a record')
+         
     return 0
 
 
 #main code
 #=======================================================================================
 #initial account balance
-balance = 0
-try:
-    balance = int(input('How many money do you have? '))
-except ValueError:
-    sys.stderr.write('Invalid value for money. Set to 0 by default.\n')
-
-
+if os.path.exists(file_path):
+    print('Welcome back!')
+else:
+    balance = 0
+    try:
+        balance = int(input('How many money do you have? '))
+    except ValueError:
+        sys.stderr.write('Invalid value for money. Set to 0 by default.\n')
+    finally:
+        write_balance(balance)
 
 
 while True:
